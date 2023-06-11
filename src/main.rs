c40bcaa42ba528b233c1554afcdc63c95622619a
Vadim Vprodus <vproduspro@gmail.com>
2023-06-11 18:53:53 +0300

    added logs to print data to terminal

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
