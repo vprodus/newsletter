632bec52cf82de8a10d0a22335a55a1ae49abb1b
Vadim Vprodus <vproduspro@gmail.com>
2023-06-11 19:11:05 +0300

    logging with id

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use sqlx::types::Uuid;
-// use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -11,6 +10,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+      "request_id {} - Adding '{}' '{}' as a new subscriber.",
+      request_id,
+      form.email,
+      form.name
+    );
+    log::info!(
+      "request_id {} - Saving new subscriber details in the database",
+      request_id
+    );
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,10 +34,20 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+          log::info!(
+            "request_id {} - New subscriber details have been saved", 
+            request_id
+            );
+          HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError().finish()
+          log::error!(
+            "request_id {} - Failed to execute query: {:?}",
+            request_id,
+            e
+          );
+          HttpResponse::InternalServerError().finish()
         }
     }
 }
