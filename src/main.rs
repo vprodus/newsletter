963460f6d7bf8a8ff0cb16d94da1c5a5ef6c5e84
Vadim Vprodus <vproduspro@gmail.com>
2023-06-11 14:24:08 +0300

    refactor the default actix server and made health_check  endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+                      .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("http://127.0.0.1:{}", port);
+    run(listener)?.await
 }
