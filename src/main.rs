193563f7b0dbdaedd62e947b9be8546e16b307cc
Vadim Vprodus <vproduspro@gmail.com>
2023-06-11 18:34:27 +0300

    configuration and postgress connection

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use newsletter::configuration::get_configuration;
-use newsletter::routes::run;
+use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if we can't read configuration
-    let configuration = get_configuration()
-                          .expect("Failed to read configuration.");
-    // We have removed the hard-coded `8000` - it's now coming from our settings! 
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(&address)
-                      .expect("Failed to bind random port");
-    println!("server runing at {}", &address);
-    run(listener)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
